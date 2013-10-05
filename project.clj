2223761db1f949427b411463fb4b98d009989a1b
Shane Kilkelly <shane@kilkelly.me>
2013-10-05 15:21:15 +0100

    tidy up the project file

--- project.clj
+++ project.clj
@@ -11,42 +11,34 @@
    [com.postspectacular/rotor "0.1.0"]
    [com.taoensso/tower "1.7.1"]
    [markdown-clj "0.9.33"]
-   [log4j
-    "1.2.17"
-    :exclusions
-    [javax.mail/mail
-     javax.jms/jms
-     com.sun.jdmk/jmxtools
-     com.sun.jmx/jmxri]]
-   [log4j
-    "1.2.17"
-    :exclusions
-    [javax.mail/mail
-     javax.jms/jms
-     com.sun.jdmk/jmxtools
-     com.sun.jmx/jmxri]]
+   [log4j "1.2.17"
+          :exclusions [javax.mail/mail
+                       javax.jms/jms
+                       com.sun.jdmk/jmxtools
+                       com.sun.jmx/jmxri]]
    [environ "0.4.0"]
    [com.novemberain/monger "1.5.0"]]
-  :ring
-  {:handler warbl.handler/war-handler,
-   :init warbl.handler/init,
-   :destroy warbl.handler/destroy}
-  :profiles
-  {:production
-    {:ring {:open-browser? false,
+
+  :ring {
+    :handler warbl.handler/war-handler,
+    :init warbl.handler/init,
+    :destroy warbl.handler/destroy}
+
+  :profiles {
+    :production {
+      :ring {:open-browser? false,
             :stacktraces? false,
             :auto-reload? false}
-    :env {:db-url "mongodb://localhost/warbl"}},
+      :env {:db-url "mongodb://localhost/warbl"}},
 
-   :dev
-    {:dependencies [[ring-mock "0.1.5"] [ring/ring-devel "1.2.0"]]
-    :env {:db-url "mongodb://localhost/warbl_local"}}
-   }
-  :url
-  "http://example.com/FIXME"
-  :plugins
-  [[lein-ring "0.8.7"]
-   [lein-environ "0.4.0"]]
+    :dev {
+      :dependencies [[ring-mock "0.1.5"]
+                     [ring/ring-devel "1.2.0"]]
+      :env {:db-url "mongodb://localhost/warbl_local"}}}
+
+  :url "http://example.com/FIXME"
+  :plugins [[lein-ring "0.8.7"]
+            [lein-environ "0.4.0"]]
   :description
   "FIXME: write description"
   :min-lein-version "2.0.0")
